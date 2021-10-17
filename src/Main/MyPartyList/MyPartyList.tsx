e9a70c1e21f8fbf0fe18767195283ebfa18bdb33
parkjbdev <parkjbdev@gmail.com>
2021-10-17 23:57:17 +0900

    Add Component: MyPartyList

---
+++ src/Main/MyPartyList/MyPartyList.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { ListGroup } from "react-bootstrap"
+import { ListGroupProps } from "react-bootstrap/ListGroup"
+
+const MyPartyList: React.FC<ListGroupProps> = props => {
+  return <ListGroup variant="flush" {...props}>{props.children}</ListGroup>
+}
+
+export default MyPartyList
---
+++ src/Main/MyPartyList/MyPartyListItem.tsx
@@ -0,0 +1,61 @@
+import React from "react"
+import { Badge, Col, Container, ListGroup, Row } from "react-bootstrap"
+import { FaUser } from "react-icons/all"
+import PartyProps from "../../interfaces/PartyProps"
+import { ListGroupItemProps } from "react-bootstrap/ListGroupItem"
+
+interface PartyListElementProps extends PartyProps {
+  bubblecount?: number
+}
+
+const MyPartyListItem: React.FC<PartyListElementProps> = props => {
+  return (
+    <Container fluid className="px-2">
+      <Row className="align-items-center h-100">
+        {/* Icon */}
+        <Col className="text-center" xs={"auto"}>
+          <FaUser />
+        </Col>
+        <Col>
+          {/* Title */}
+          <Row className="fw-bold m-1">{props.name}</Row>
+          {/* Content */}
+          <Row className="fw-light m-1">
+            <small className="p-0">{props.content}</small>
+          </Row>
+        </Col>
+        
+        <Col className="text-end p-0" xs={"auto"}>
+          {/* Date */}
+          <Row>
+            <small>{props.date.toLocaleDateString()}</small>
+          </Row>
+          {/* Bubble */}
+          <Row>
+            {props.bubblecount === 0 ? (
+              <small>&nbsp;</small>
+            ) : (
+              <small>
+                <Badge pill bg="primary">
+                  {props.bubblecount}
+                </Badge>
+              </small>
+            )}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+MyPartyListItem.defaultProps = { bubblecount: 0 }
+
+const MyPartyListItemWrapper: React.FC<PartyListElementProps & ListGroupItemProps> = props => {
+  return (
+    <ListGroup.Item {...props}>
+      <MyPartyListItem {...props} />
+    </ListGroup.Item>
+  )
+}
+
+export default MyPartyListItemWrapper
