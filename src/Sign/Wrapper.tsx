316648ebbc4a8f7bca85b4802f3388ee1d3ab022
parkjbdev <parkjbdev@gmail.com>
2021-12-27 17:08:45 +0900

    fix: Conditional Overflow of AnimatedForm Depending on Window Height

--- src/Sign/Wrapper.tsx
+++ src/Sign/Wrapper.tsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./Wrapper.scss"
 import { Col, Container, Row } from "react-bootstrap"
 import { motion } from "framer-motion"
+import useWindowDimensions from "../utils/useWindowDimensions"
 
 const Wrapper: React.FC<React.HTMLAttributes<HTMLElement> & { hero: React.ReactElement; form: React.ReactElement }> =
   props => {
+    // FIXME: Only overflow-auto for AnimatedForm Element
+    const { height } = useWindowDimensions()
+    const overflowRef = React.useRef(null)
+    const [isOverflow, setIsOverflow] = useState(true)
+
+    useEffect(() => {
+      if (!overflowRef.current) return
+      const obj: HTMLElement = overflowRef.current
+      setIsOverflow(obj.scrollHeight > obj.clientHeight)
+    }, [height]);
+
     const AnimatedHero = <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
       {props.hero}
     </motion.div>
@@ -15,14 +27,14 @@ const Wrapper: React.FC<React.HTMLAttributes<HTMLElement> & { hero: React.ReactE
     return (
       <>
         <Container fluid className="d-none d-md-block p-0 vh-100">
-          <Row className="m-0 h-100">
+          <Row className="m-0 h-100" ref={overflowRef}>
             <Col className="hero p-0 position-relative" md={8}>
               <Container fluid className="position-absolute top-50 translate-middle-y">
                 {AnimatedHero}
               </Container>
             </Col>
-            <Col className="form-col p-0 position-relative overflow-auto">
-              <Container fluid className="position-absolute top-50 translate-middle-y">
+            <Col className={`form-col p-0 position-relative`}>
+              <Container fluid className={`position-absolute${!isOverflow && " top-50 translate-middle-y"}`}>
                 {AnimatedForm}
               </Container>
             </Col>
