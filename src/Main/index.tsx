0bbb9f08be5e26f9d35f0bd3a5094e9ddf52c17c
parkjbdev <parkjbdev@gmail.com>
2021-12-31 13:58:29 +0900

    feat: Add TableView

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,7 +1,7 @@
-import { Col, Container, Row } from "react-bootstrap"
-import { FiSearch } from "react-icons/fi"
+import { Col, Container, Row, Table } from "react-bootstrap"
+import { FaUser } from "react-icons/fa"
+import { FiMoreHorizontal, FiSearch } from "react-icons/fi"
 import { GiPartyHat } from "react-icons/gi"
-import "./index.scss"
 import Navbar from "./Navbar"
 import PartyCard from "./PartyCardGrid/PartyCard"
 import PartyCardGroup from "./PartyCardGrid/PartyCardGroup"
@@ -10,6 +10,38 @@ import MyParty from "./Sidebar/MyParty"
 import SearchBar from "./Sidebar/Search"
 
 function Index() {
+  const TableRow = () => {
+    return <tr>
+      <td className="align-middle">
+        <Row className="px-3 align-items-center">
+          <Col className="text-center" xs={"auto"}>
+            <FaUser />
+          </Col>
+
+          <Col>
+            {/* Title */}
+            <Row className="fw-bold m-1">{"파티이름"}</Row>
+            {/* Content */}
+            <Row className="fw-light m-1">
+              <small className="p-0">{"파티 info"}</small>
+            </Row>
+          </Col>
+        </Row>
+      </td>
+      <td className="align-middle">
+        { }
+      </td>
+      <td className="align-middle">3/5</td>
+      <td className="align-middle">
+        <button className="mx-1 btn btn-light btn-sm"><small>POPULAR</small></button>
+        <button className="mx-1 btn btn-light btn-sm"><small>NEW</small></button>
+        <button className="mx-1 btn btn-light btn-sm"><small>JOB</small></button>
+      </td>
+      <td className="align-middle">09 SEP 2017</td>
+      <td className="align-middle"><FiMoreHorizontal /></td>
+    </tr >
+  }
+
   return (
     <Container fluid className="d-flex flex-column vh-100">
       {/* Navbar */}
@@ -21,18 +53,18 @@ function Index() {
 
         {/* SIDEBAR CONTROL */}
         <SideBar>
-          <SideBarItem value={0} icon={<GiPartyHat />} children={<MyParty />} />
-          <SideBarItem value={1} icon={<FiSearch />} children={<SearchBar />} />
+          <SideBarItem icon={<GiPartyHat />} children={<MyParty />} />
+          <SideBarItem icon={<FiSearch />} children={<SearchBar />} />
         </SideBar>
 
         {/* END OF SIDEBAR CONTROL */}
 
 
-        <Col className="pt-4 border-start border-1">
+        <Col className="pt-4 px-5 border-start border-1">
           <Container fluid>
             <h3 className="ms-4 mb-4 fw-bold">지금 구하고 있어요</h3>
 
-            <PartyCardGroup className="p-0">
+            <PartyCardGroup className="p-0 mb-5 ">
               <PartyCard />
               <PartyCard />
               <PartyCard
@@ -43,9 +75,39 @@ function Index() {
             </PartyCardGroup>
           </Container>
 
-          <Row>
-            {/* TODO: 게시글 목록 */}
-          </Row>
+          <Container fluid>
+            <Table borderless hover className="p-0 ">
+              <colgroup>
+                <col span={1} style={{ width: "20%" }} />
+                <col span={1} style={{ width: "10%" }} />
+                <col span={1} style={{ width: "10%" }} />
+                <col span={1} style={{ width: "15%" }} />
+                <col span={1} style={{ width: "10%" }} />
+                <col span={1} style={{ width: "5%" }} />
+              </colgroup>
+
+              <thead>
+                <tr>
+                  <th className="fw-lighter" style={{ fontSize: "10px" }}>BASIC INFO</th>
+                  <th className="fw-lighter" style={{ fontSize: "10px" }}>파티장</th>
+                  <th className="fw-lighter" style={{ fontSize: "10px" }}>CURRENT MEMBER</th>
+                  <th className="fw-lighter" style={{ fontSize: "10px" }}>TAGS</th>
+                  <th className="fw-lighter" style={{ fontSize: "10px" }}>CREATED DATE</th>
+                  <th className="fw-lighter" style={{ fontSize: "10px" }}>더보기</th>
+                </tr>
+              </thead>
+              <tbody>
+                <TableRow />
+                <TableRow />
+                <TableRow />
+                <TableRow />
+                <TableRow />
+                <TableRow />
+                <TableRow />
+                <TableRow />
+              </tbody>
+            </Table>
+          </Container>
 
         </Col>
 
